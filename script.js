d64e0a2e9a9d249a94456c7ef5fb2408b8129261
Glitch (plausible-noiseless-honeycup) <none>
2023-07-23 10:40:49 +0000

    👋🌡️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,80 +1,69 @@
+let playerScore = 0;
+let computerScore = 0;
 
-let playerScore;
-let computerScore;
+const GAME_LOST = "Too Bad! Computer won this game!";
+const GAME_WON = "Congratulations! You won this game!";
 
-computerWinsMsg = ("Too Bad! Computer won this game!");
-playerWinsMsg = ("Congratulations! You won this game!");
-
-ROUND_WON = ("You are a winner!");
-ROUND_LOST = ("You lost!");
-TIE_GAME = ("It's a tie!");
+const USER_ROUND_WON = "You are a winner!";
+const USER_ROUND_LOST = "You lost!";
+const USER_ROUND_TIE = "It's a tie!";
 
 
-function getComputerChoice () {
-    const computerSelection = ["rock", "paper", "scissors"];
-    return computerSelection[Math.floor(Math.random() * computerSelection.length)];
-
-} 
+function getComputerChoice() {
+  const computerSelection = ["rock", "paper", "scissors"];
+  return computerSelection[
+    Math.floor(Math.random() * computerSelection.length)
+  ];
+}
 
 // console.log(getComputerChoice());
 
-
-function getPlayerSelection () {
-    let playerSelectionPrompt = prompt("Choose your weapon: ROCK, PAPER or SCISSORS!").toLowerCase();
-    return playerSelectionPrompt;
+function getPlayerSelection() {
+  let playerSelectionPrompt = prompt(
+    "Choose your weapon: ROCK, PAPER or SCISSORS!"
+  ).toLowerCase();
+  return playerSelectionPrompt;
 }
 
 // console.log(getPlayerSelection());
 
+function playRound() {
+  let playerSelection = getPlayerSelection();
+  let computerSelection = getComputerChoice();
 
-function playRound (playerSelection, computerSelection) {
-
-    playerSelection = getPlayerSelection();
-    computerSelection = getComputerChoice();
-    
-    if (playerSelection === computerSelection) {
-        return TIE_GAME;
-    }
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return (`${ROUND_WON} Rock beats scissors!`);
-    }
-    if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return (`${ROUND_WON} Paper beats rock!`);
-    }
-    if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return (`${ROUND_WON} Scissors beats paper!`);
-    }
-    else {
-        return ROUND_LOST;
-    }
-
-
+  if (playerSelection === computerSelection) {
+    return USER_ROUND_TIE;
+  }
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return USER_ROUND_WON;
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return USER_ROUND_WON;
+  }
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    return USER_ROUND_WON;
+  }
+  return USER_ROUND_LOST;
 }
 
 // console.log(playRound());
 
-
-
 function game() {
-    
-    for (let i = 0; i < 5; i++) {
-        const playedRound = playRound();
-        
-        console.log(playedRound);
-    }
+  for (let i = 0; i < 5; i++) {
+    const roundResult = playRound();
+
+    console.log(roundResult);
+  }
 }
 
 console.log(game());
 
-
-function declareWinner() {
-    if (game() === ROUND_WON) {
-        playerScore += 0;
-    }
-    if (game() === ROUND_LOST) {
-        computerScore += 0;
-        
-    }
+function decideRoundWinner(roundResult) {
+  if(roundResult === USER_ROUND_WON) {
+    playerScore += 1;
+  }
 }
 
-console.log(declareWinner());
+function decideGameWinner() {
+
+}
